67f77f5ed3991c60900ca18f80887d5f3f79996a
Robert Alfrey <robertalfrey34@gmail.com>
2023-12-11 23:30:32 -0600

    Implemented a process of encrypting the file while the program is not in use encryption uses Fernet from cryptography module

--- passwordBank.py
+++ passwordBank.py
@@ -1,3 +1,5 @@
+from cryptography.fernet import Fernet
+
 #Global options
 ADDENTRY = 1
 VIEW = 2
@@ -5,14 +7,16 @@ LOAD = 3
 SAVE = 4
 QUIT = 5
 
+
+
 def main():
     #Gets users choice, and calls apropriate function
     
     currentPassDict = loadFromFile()
 
     userChoice = 0
-    
-    while userChoice != QUIT:
+    running = True
+    while running:
         userChoice = getUserChoice()
         
         if userChoice == ADDENTRY:
@@ -23,6 +27,10 @@ def main():
             saveToFile(currentPassDict)
         if userChoice == VIEW:
             viewDatabase(currentPassDict)
+        if userChoice == QUIT:
+            saveToFile(currentPassDict)
+            encryptFile('key.key', 'passList.txt')
+            running = False
 
 def getUserChoice():
     #prints users choices and returns the users choice
@@ -62,7 +70,6 @@ def addEntry(passDict):
     passDict[usedFor] = userAndPass
     return passDict
     
-
 def saveToFile(DictToSave):
     #Saves DictToSave to passlist.txt
     print()
@@ -79,15 +86,64 @@ def saveToFile(DictToSave):
     if procceed == True:
         with open('passList.txt', 'w') as passFile:
             passFile.write(str(DictToSave))
-
+            
+            
+            
 def loadFromFile():
     #loads passList.txt
     print()
     print("Loading current passlist.txt")
+       
+    decryptFile('key.key', 'passList.txt')
     
     with open('passList.txt', 'r') as passFile:
         data = passFile.read()
         return eval(data)
+
+def generateKeyFile():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb+') as keyFile:
+        keyFile.write(key)
         
+def loadKey(keyFile):
+    with open(keyFile, 'rb+') as keyFile:
+        key = keyFile.read()
+    return key
+
+def encrypt(keyFile, string):
+    key = loadKey(keyFile)
+    f = Fernet(key)
     
+    string = string.encode('UTF-8')
+    token = f.encrypt(string)
+    
+    return token
+
+def decrypt(keyFile, token):
+    key = loadKey(keyFile)
+    f = Fernet(key)
+    
+    string = f.decrypt(token)
+    string = string[2:-1]
+    
+    return string
+
+def encryptFile(keyFile, file):
+    with open(file, 'rb+') as myFile:
+        secret = str(myFile.read())
+        token = encrypt(keyFile, secret)
+        
+        myFile.truncate(0)
+        myFile.seek(0)
+        myFile.write(token)
+        
+def decryptFile(keyFile, file):
+    with open(file, 'rb+') as myFile:
+        token = myFile.read()
+        secret = decrypt(keyFile, token)
+        secret = secret.decode('UTF-8')
+    
+    with open(file, 'w') as myFile:
+        myFile.write(str(secret))
+
 main()
